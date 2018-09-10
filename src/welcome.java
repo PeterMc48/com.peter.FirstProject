fc77863a6da95f916bea7a6ad2cfdca4f2e14d17
Peter A. Mc Cafferty <t00095451@365s.ittralee.ie>
2018-09-10 17:20:25 +0100

    Initial commit

---
+++ src/welcome.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class welcome {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("");
+        JOptionPane.showMessageDialog(null,"welcome to year two "+name);
+
+
+
+    }
+}
